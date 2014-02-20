7a1130b1ab7375903030e6d97b5569469b76e3fa
Michael Ball <cycomachead@gmail.com>
2014-02-20 00:47:37 -0800

    some tweaks to script

--- panda/panda.py
+++ panda/panda.py
@@ -3,6 +3,9 @@
 import sys
 import csv
 
+scores = []
+# TODO Parameter for Questions to drop, start and end
+# Parameter for sorting.
 if __name__ == '__main__':
     if not sys.argv[1]:
         print("No File Given")
@@ -13,12 +16,13 @@ if __name__ == '__main__':
     grades = open(gr, "r")
     gr = csv.reader(grades)
     print("%36s |  %9s | %4s" % ("NAME", "SID", "DROP POINTS"))
-    print("---------------------------------------------------------------")
+    print('\t' + "-" * 54)
     for row in gr:
         # print(row)
         name = row[0]
         if name == "Name":
             continue
         sid  = row[1]
-        reading = row[6:17]
+        reading = row[6:11]
+        scores.append((name, sid, min(reading)))
         print("%36s |  %9s | %4s" % (name, sid, min(reading)))
