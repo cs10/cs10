1c06ad2127b69ebaca2bf7d26be77fde45596730
Michael Ball <cycomachead@gmail.com>
2015-10-03 00:16:48 -0700

    Add new command line args to panda

--- panda/panda.py
+++ panda/panda.py
@@ -2,45 +2,54 @@
 
 import sys
 import csv
+from heapq import nsmallest
 
 scores = []
 # TODO Parameter for Questions to drop, start and end
 # Parameter for sorting.
 if __name__ == '__main__':
-    if not sys.argv[1]:
-        print("No File Given")
-        sys.exit(0)
+    if len(sys.argv) < 2:
+        print("Error No File or drop count Given")
+        print("Usage:\npanda.py FILE.CSV DROP-QUESTIONS [drop-number]")
+        print("Drop Questions is the number of questions to count in dropping.")
+        print("This current starts from 1 and goes to N.")
+        print("Drop Number defaults to 1, otherwise should be an integer.")
+        sys.exit(1)
     print("PANDA GRADER DROP SCORE CALCULATOR\n")
-    gr = sys.argv[1]
-    print('\t' + gr)
-    drop = 6
+    file = sys.argv[1]
+    print('\t' + file)
+    DROP_Qs = 6
     if len(sys.argv) > 2:
-        drop = int(float(sys.argv[2]))
-    grades = open(gr, "r")
-    gr = csv.reader(grades)
+        DROP_Qs = int(float(sys.argv[2]))
+    TODROP = 1
+    if len(sys.argv) > 3:
+        TODROP = int(float(sys.argv[3]))
+    grades = open(file, "r")
+    grade_data = csv.reader(grades)
     print('\t' + "-" * 54)
-    print("%36s |  %9s |  %4s" % ("NAME", "SID", "DROP"))
+    print("%36s |  %9s |  %10s | %8s" % ("NAME", "SID", "DROP SCORE", "INDIV Q SCORES"))
     print('\t' + "-" * 54)
-    for row in gr:
+    for row in grade_data:
         # print(row)
         name = row[0]
         if name == "Name": # HEADER
-            print(row[7:7 + drop]) # Verify the question titles
+            print(row[7:7 + DROP_Qs]) # Verify the question titles
             continue
         sid  = row[1]
-        reading = row[7:(7 + drop   )] # FIXME...
+        reading = row[7:7 + DROP_Qs] # FIXME...
         # Merge reading scores with two parts.
         # quest
         # 1: 2 parts, 2: 2 parts
         # reading[0:2] = [ (float(reading[0]) + float(reading[1])) ]
         # reading[1:3] = [ (float(reading[1]) + float(reading[2])) ]
         reading = list(map(float, reading))
-        minScore = min(reading)
-        scores.append((name, sid, minScore))
-        print("%36s |  %9s | %4s" % (name, sid, minScore))
+        minScores = nsmallest(TODROP, reading)
+        dropScore = sum(minScores)
+        scores.append((name, sid, dropScore, minScores))
+        print("%36s |  %9s | %10s | %8s" % (name, sid, dropScore, minScores))
     print('\t' + ('=' * 54))
     print('\tSCORES NOT ZERO')
     scores.sort(key=lambda i: i[0]) # Sort via name
     for s in scores:
         if float(s[2]) > 0:
-            print("%36s |  %9s | %4s" % (s[0], s[1], s[2]))
+            print("%36s |  %9s | %10s | %8s" % (s[0], s[1], s[2], s[3]))
